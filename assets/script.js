c37f5aafc7e927ef4d4cb074da93bada4e18ef8b
Jlvstrasse <jlvstrasse@gmail.com>
2024-03-29 13:20:13 -0400

    $ space fixed

--- assets/script.js
+++ assets/script.js
@@ -47,7 +47,7 @@ const displayAverageSalary = function(employeesArray) {
 
   const averageSalary = total / employeesArray.length;
   
-  console.log(`The average employee salary between our ${employeesArray.length} employee(s) is $ ${averageSalary.toFixed(2)}`);
+  console.log(`The average employee salary between our ${employeesArray.length} employee(s) is $${averageSalary.toFixed(2)}`);
 }
 
 
